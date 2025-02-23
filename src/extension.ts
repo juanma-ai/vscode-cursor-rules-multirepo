55890913c54d305c9ad74321cc29e470f1a4528f
JuanMa Garrido <juanma.garrido@gmail.com>
2025-02-23 06:05:03 +0000

    removed logic that was creating vscode/settings.json on every project

--- src/extension.ts
+++ src/extension.ts
@@ -5,14 +5,6 @@ import { Cache } from "./utils/cache";
 export function activate(context: vscode.ExtensionContext) {
   Cache.getInstance(context);
 
-  // Force configuration reload
-  const config = vscode.workspace.getConfiguration("cursorRules");
-  config.update(
-    "repos",
-    config.get("repos"),
-    vscode.ConfigurationTarget.Workspace
-  );
-
   let disposable = vscode.commands.registerCommand(
     "cursorRules.addRule",
     () => {
